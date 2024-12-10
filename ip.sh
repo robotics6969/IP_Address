7301c72ca48cfeb648fc91aee0fbebfc8d0429fe
Beagle User <debian@BeagleBone.localdomain>
2024-12-10 12:19:24 +0000

    added ip

--- ip.sh
+++ ip.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}'>ip.txt
+ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}'>README.md
 git add .
 git commit -m "added ip"
 git push
